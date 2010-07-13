2801a2067674146741d02aee1b8731b35775b130
Peter Higgins <dante@adcsugr.local>
2010-07-13 13:26:44 -0400

    adding in inline docs and refactoring code to be smaller and work more as if you were working with a dojo.deferred

--- dojox/yql/_base.js
+++ dojox/yql/_base.js
@@ -1,67 +1,88 @@
 dojo.provide("dojox.yql._base");
-
 dojo.require("dojo.io.script");
-
 /*
  * Begin Yahoo Web Services Attribution Snippet
  * http://developer.yahoo.com - Web Services by Yahoo!
  * End Yahoo Web Services Attribution Snippet
  */
-(function() {
-   var _dio = dojo.io;
-   var _dxy = dojox.yql;
-   var URL = 'http:/' + '/query.yahooapis.com/v1/public/yql';
-   _dxy.execute = function( qry, ioArgs ) {
+(function(d){
 
-     if (!ioArgs) {
-       ioArgs = { };
-     }
-     var myArgs = dojo.mixin({},ioArgs );
-     if (!myArgs.content) {
-       myArgs.content = { };
-     }
-     myArgs.url = URL;
-     myArgs.callbackParamName = "callback";
-     myArgs.content.q = qry;
-     myArgs.content.format = 'json';
+    var URL = 'http:/' + '/query.yahooapis.com/v1/public/yql';
 
-     if(myArgs.diagnostics) {
-       if (dojo.isString(myArgs.diagnostics)) {
-         myArgs.content.diagnostics = (myArgs.diagnostics === "true");
-       }
-       else {
-         // it's true
-         myArgs.content.diagnostics = true;
-       }
-     }
-     else {
-       // false by default (or set already to false)
-       myArgs.content.diagnostics = false;
-     }
+    dojox.yql = function(query, ioArgs){
+        // summary: An interface to Yahoo's YQL web service
+        //
+        // description:
+        //      An interface to Yahoo's YQL web service. See 
+        //      http://developer.yahoo.com/yql/ for full information
+        //      and to obtain the an API key. 
+        //
+        // query: String
+        //      Some YQL query to execute. eg: "select * from internet"
+        //
+        // ioArgs: Object?
+        //      A Standard Dojo XHR ioArgs object, with special consideration:
+        //
+        //        * No `url` needs specified as it it hardcoded to point to YAHOO's
+        //        service. 
+        //        * Any other options should behave as expected, though
+        //        for the sake of not breaking the interface it is suggested you
+        //        avoid using the `form` parameters. 
+        //        * `ioArgs.diagnostics` can be passed to trigger diagnostic information (?)
+        //        * `ioArgs.env` can be passed to supply a different datatable evn (?)
+        //
+        // example:
+        //  |   var handleIt = function(data){ console.warn(data); };
+        //  |   dojox.yql("select * from internet").addCallback(handleIt);
+        //
+        // example:
+        //  |   dojox.yql("select geo.places where text = 'SFO'", {
+        //  |       load: function(data){ ... }
+        //  |   });
+        //
+        // example:
+        //  |   dojox.yql("select * from internet", {
+        //  |       error: function(e){ ... }
+        //  |   }).addCallback(handleIt);
+        //
+        // example:
+        //  |   dojox.yql("select * from internet", {
+        //  |       timeout:5000,
+        //  |       error: function(e){},
+        //  |       load: function(e){}
+        //  |   });
+        //
+        //  example: 
+        //  |   dojox.yql("select it").addBoth(function(dataOrError){ ... });
+        
+        var args = d.mixin({
+            url: URL,
+            callbackParamName: "callback",
+            format:"json",
+            q: query,
+            content: {}
+        }, ioArgs || {});
+        
+        args.content.diagnostics = args.diagnostics && args.diagnostics === "true" || args.diagnostics === true;
+        args.content.env = args.content.env || args.env || 'http:/' + '/datatables.org/alltables.env';
+        
+        // we need to make a new Deferred because we want to regulate the callbacks passed
+        // in `ioArgs`. Attach those to this new deferred, which will be returned.
+        var dfd = new d.Deferred();
+        args.load && dfd.addCallback(args.load);
+        args.error && dfd.addErrback(args.error);
+        args.handle && dfd.addBoth(args.handle);
+        
+        // kill old load/error function if any at all so we can fire err/callback based on data.error,
+        // as json-p doesn't have traditional errors (though 404's etc will throw methinks?)
+        args.load = function(data){ dfd[(data.error ? "err" : "call") + "back"](data); }
+        args.error = function(e){ dfd.errback(e); }
+        args.handle = null; // sokay, we bound an original before here
+        
+        d.io.script.get(args);
+        
+        return dfd;
 
-     if (myArgs.env) {
-       myArgs.content.env = myArgs.env;
-     } else {
-       if (!myArgs.content.env) {
-         myArgs.content.env = 'http:/' + '/datatables.org/alltables.env';
-       }
-     }
-
-     // to look into the data to see if there's an error
-     myArgs.load = function( data ) {
-       if (data.error) {
-         if (ioArgs.error) {
-           ioArgs.error( new Error( data.error.description ) );
-           return;
-         }
-       }
-       if (data.query) {
-         if (ioArgs.load) {
-           ioArgs.load( data.query );
-           return;
-         }
-       }
-     };
-     return _dio.script.get( myArgs );
     };
- })();
+
+})(dojo);
